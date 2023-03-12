ea3338119df90346c08c6dbef422d77103ea62ce
PabloSanttana <pablo_guilherme02@hotmail.com>
2023-03-11 21:31:27 -0300

    create models

--- project/settings.py
+++ project/settings.py
@@ -130,6 +130,10 @@ STATICFILES_DIRS = [
 # commd  python manage.py collectstatic
 STATIC_ROOT = BASE_DIR / "static"
 
+# upload de imgs
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from __localcode.main import make_recipe
 # Create your views here.
 
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Pablo santana'
+        'recipes': [make_recipe() for _ in range(10)]
     })
 
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Pablo santana'
+        'recipe': make_recipe(),
+        'is_detail_page': True
     })

2ac0b3d5f5d7d9b76e4413426f2f985b6a0673d1
PabloSanttana <pablo_guilherme02@hotmail.com>
2023-10-31 15:40:16 -0300

    create page category

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,5 +6,7 @@ app_name = 'recipes'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('recipes/<int:id>/', views.recipes, name='details')
+    path('recipes/category/<int:category_id>/',
+         views.category, name='category'),
+    path('recipes/<int:id>/', views.recipes, name='details'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 
-from utils.factory import make_recipe
+from recipes.models import Recipe
+
+# from utils.factory import make_recipe
 
 
 def home(request):
+    recipes = Recipe.objects.filter(is_published=True).order_by("-id")
     return render(request, 'recipes/views/home.html', {
-        "recipes": [make_recipe() for _ in range(10)]
+        "recipes": recipes
+    })
+
+
+def category(request, category_id):
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True
+        ).order_by("-id")
+    )
+
+    return render(request, 'recipes/views/category.html', {
+        "recipes": recipes,
+        "title": f'{recipes[0].category.name}- Category'
     })
 
 
 def recipes(request, id):
+    recipe = get_object_or_404(Recipe, pk=id,  is_published=True)
+
     return render(request, 'recipes/views/recipes_view.html', {
-        "recipe": make_recipe(),
+        "recipe": recipe,
         "is_detail_page": True
     })

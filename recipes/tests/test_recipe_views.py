93884678d5cfb2b94dc46aad2457e824244af894
PabloSanttana <pablo_guilherme02@hotmail.com>
2023-11-08 15:32:22 -0300

    continuando os teste

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,4 +1,3 @@
-from django.test import TestCase
 from django.urls import resolve, reverse
 
 from recipes import views
@@ -47,7 +46,7 @@ class RecipeViewHomeTest(RecipeTestBase):
         self.assertIn('Pablo', content)
 
 
-class RecipeViewDetailTest(TestCase):
+class RecipeViewDetailTest(RecipeTestBase):
     def test_recipe_detail_view_is_correct(self):
         view = resolve(
             reverse('recipes:details', kwargs={'id': 1})
@@ -60,8 +59,26 @@ class RecipeViewDetailTest(TestCase):
         )
         self.assertEqual(response.status_code, 404)
 
+    def test_recipe_detail_template_load_is_correct_recipe(self):
+        # create recipes antes do template
+        title = "this is a detail page - it load one recipe"
+        create_recipe = self.make_recipe(title=title)
+        response = self.client.get(
+            reverse('recipes:details', kwargs={'id': 1})
+        )
 
-class RecipeViewCategoryTest(TestCase):
+        recipe = response.context["recipe"]
+        # usando context
+        self.assertEqual(create_recipe.title, recipe.title)
+
+        # usando content
+        content = response.content.decode('utf-8')
+
+        self.assertIn(title, content)
+        self.assertIn('sanatana', content)
+
+
+class RecipeViewCategoryTest(RecipeTestBase):
     def test_recipe_category_view_is_correct(self):
         view = resolve(
             reverse('recipes:category', kwargs={'category_id': 1})
@@ -73,3 +90,24 @@ class RecipeViewCategoryTest(TestCase):
             reverse('recipes:category', kwargs={'category_id': 1000})
         )
         self.assertEqual(response.status_code, 404)
+
+    def test_recipe_category_template_loads_the_correct_recipes(self):
+        # create recipes antes do template
+        title = "This is a category test"
+        create_recipe = self.make_recipe(title=title)
+        response = self.client.get(
+            reverse('recipes:category', kwargs={'category_id': 1})
+        )
+
+        recipes = response.context["recipes"]
+        recipes
+        # usando context
+        self.assertEqual(len(recipes), 1)
+        self.assertEqual(create_recipe.title, recipes[0].title)
+
+        # usando content
+        content = response.content.decode('utf-8')
+
+        self.assertIn(title, content)
+        self.assertIn('sanatana', content)
+        self.assertIn('carnes', content)

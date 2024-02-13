5aa71a73a3059566a1ad537ed009bb0578bcdf97
PabloSanttana <pablo_guilherme02@hotmail.com>
2024-02-13 16:41:56 -0300

    selenium navegacao

--- tests/funcional_tests/recipes/test_recipe_home_page.py
+++ tests/funcional_tests/recipes/test_recipe_home_page.py
@@ -46,3 +46,27 @@ class RecipeHomeFunctionTestCase(RecipeBaseFunctionalTest):
             By.CLASS_NAME, 'recipe-title')
 
         self.assertIn(title_needed, content_title.text)
+
+    @patch('recipes.views.PER_PAGE', new=2)
+    def test_recipe_page_pagination(self):
+        # Criar cinco receitas
+        self.make_recipe_in_batch(5)
+
+        # Usuário abre a página e verifica a paginação
+        self.browser.get(self.live_server_url)
+
+        # Verifica se há uma página 2
+        page2 = self.browser.find_element(
+            By.XPATH, '//a[@aria-label="Go to page 2"]')
+
+        # Usuário clica na página 2 para ver as receitas daquela página
+        page2.click()
+
+        # Verifica se o usuário foi para a página 2
+        current_page = self.browser.find_element(
+            By.XPATH, '//a[@aria-label="Current page, 2"]')
+        self.assertEqual('2', current_page.text)
+
+        # Verifica se há mais duas receitas na página 2
+        recipes = self.browser.find_elements(By.CLASS_NAME, 'recipe-list-item')
+        self.assertEqual(2, len(recipes))
--- utils/pagination.py
+++ utils/pagination.py
@@ -3,9 +3,12 @@ import math
 from django.core.paginator import Paginator
 
 
-def make_pagination_range(page_range, qty_pages, current_page):
-
-    middle_range = math.ceil(qty_pages/2)
+def make_pagination_range(
+    page_range,
+    qty_pages,
+    current_page,
+):
+    middle_range = math.ceil(qty_pages / 2)
     start_range = current_page - middle_range
     stop_range = current_page + middle_range
     total_pages = len(page_range)
@@ -17,18 +20,19 @@ def make_pagination_range(page_range, qty_pages, current_page):
         stop_range += start_range_offset
 
     if stop_range >= total_pages:
-        start_range = start_range - abs(total_pages - stop_range)
+        start_range = max(start_range - abs(total_pages - stop_range), 0)
 
     pagination = page_range[start_range:stop_range]
     return {
         'pagination': pagination,
-        'current_page': current_page,
         'page_range': page_range,
+        'qty_pages': qty_pages,
+        'current_page': current_page,
         'total_pages': total_pages,
         'start_range': start_range,
         'stop_range': stop_range,
         'first_page_out_of_range': current_page > middle_range,
-        'last_page_out_of_range': stop_range < total_pages
+        'last_page_out_of_range': stop_range < total_pages,
     }
 
 

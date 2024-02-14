a2c96cd8ea5c998fe03843d39487e2acdd63e258
PabloSanttana <pablo_guilherme02@hotmail.com>
2024-02-14 17:37:06 -0300

    teste app authors completo com unittest e seleniun

--- tests/funcional_tests/authors/test_authors_login.py
+++ tests/funcional_tests/authors/test_authors_login.py
@@ -12,34 +12,25 @@ from .test_base import AuthorsBaseFunctionalTest
 @pytest.mark.functional_test
 class AuthorsLoginFunctionalTest(AuthorsBaseFunctionalTest):
 
-    def make_create_user(self):
-        User.objects.create_user(
-            username='rafaelaDarc', password='Abc123456')
-
-    def test_authors_login_valid_data_succes(self):
-        self.make_create_user()
-        self.browser.get(self.live_server_url + reverse('authors:login'))
-
-        form = self.browser.find_element(By.ID, 'login_user')
-
-        form.find_element(By.NAME, 'username').send_keys('rafaelaDarc')
-        form.find_element(By.NAME, 'password').send_keys('Abc123456')
-        form.submit()
-        msg = 'Your are logged in.'
-
-        body = self.browser.find_element(By.TAG_NAME, 'body')
-
-        self.assertIn(msg, body.text)
+    def make_create_author(self):
+        # cria um usuario
+        password = 'Ab123456789'
+        user = User.objects.create_user(
+            username='RafaelaDarc',
+            last_name='Darc',
+            first_name='Rafaela',
+            email='RafaelaDarc@gmail.com',
+            password=password)
+        return (user.username, password)
 
     def test_authors_login_invalid_credentials(self):
 
-        self.make_create_user()
         self.browser.get(self.live_server_url + reverse('authors:login'))
 
         form = self.browser.find_element(By.ID, 'login_user')
 
-        form.find_element(By.NAME, 'username').send_keys('rafaelaDarck')
-        form.find_element(By.NAME, 'password').send_keys('Abc1234561')
+        form.find_element(By.NAME, 'username').send_keys('RafaelaDarc')
+        form.find_element(By.NAME, 'password').send_keys('Ab123456789')
         form.submit()
         msg = 'Invalid credentials'
 
@@ -61,3 +52,59 @@ class AuthorsLoginFunctionalTest(AuthorsBaseFunctionalTest):
         body = self.browser.find_element(By.TAG_NAME, 'body')
 
         self.assertIn(msg, body.text)
+
+    def test_authors_login_valid_data_succes(self):
+        # Obtém as credenciais de um usuário criado anteriormente
+        username, password = self.make_create_author()
+
+        # Navega até a página de login
+        self.browser.get(self.live_server_url + reverse('authors:login'))
+
+        # Localiza o formulário de login na página
+        form = self.browser.find_element(By.ID, 'login_user')
+
+        # Preenche o nome de usuário e a senha nos campos
+        # de entrada do formulário
+        form.find_element(By.NAME, 'username').send_keys(username)
+        form.find_element(By.NAME, 'password').send_keys(password)
+
+        # Submete o formulário para fazer login
+        form.submit()
+
+        # Verifica se a mensagem 'Your are logged in.'
+        # está presente no corpo da página
+        # Isso indica que o usuário foi deslogado com sucesso
+        msg = 'Your are logged in.'
+
+        body = self.browser.find_element(By.TAG_NAME, 'body')
+
+        self.assertIn(msg, body.text)
+
+    def test_authors_logout_success(self):
+        # Obtém as credenciais de um usuário criado anteriormente
+        username, password = self.make_create_author()
+
+        # Navega até a página de login
+        self.browser.get(self.live_server_url + reverse('authors:login'))
+
+        # Localiza o formulário de login na página
+        form = self.browser.find_element(By.ID, 'login_user')
+
+        # Preenche o nome de usuário e a senha
+        # nos campos de entrada do formulário
+        form.find_element(By.NAME, 'username').send_keys(username)
+        form.find_element(By.NAME, 'password').send_keys(password)
+
+        # Submete o formulário para fazer login
+        form.submit()
+
+        # Localiza e submete o formulário de logout
+        form = self.browser.find_element(By.ID, 'form_logout')
+        form.submit()
+
+        # Verifica se a mensagem 'Your are logged in.'
+        # não está presente no corpo da página
+        # Isso indica que o usuário foi deslogado com sucesso
+        msg = 'Your are logged in.'
+        body = self.browser.find_element(By.TAG_NAME, 'body')
+        self.assertNotIn(msg, body.text)

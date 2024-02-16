28a72f9e16d4578c44a03d489c921b8b02d0283b
PabloSanttana <pablo_guilherme02@hotmail.com>
2024-02-15 22:15:04 -0300

    area admin customização

--- recipes/admin.py
+++ recipes/admin.py
@@ -12,6 +12,13 @@ class CategoryAdmin(admin.ModelAdmin):
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
+    list_display = ['id', 'title', 'created_at', 'is_published', 'author']
+    list_display_links = ['id', 'title', 'created_at',]
+    search_fields = ['id', 'title', 'description', 'preparation_steps']
+    list_filter = ['category', 'author', 'preparation_steps_is_html']
+    list_per_page = 20
+    list_editable = ['is_published',]
+    ordering = ['-id',]
 
 
 admin.site.register(Category, CategoryAdmin)
